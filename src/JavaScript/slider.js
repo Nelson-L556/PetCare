967574fb6d14cb76f8c3ef142cf1fd53bffeea3b
Nelson Lara <nelson.lara556@hotmail.com>
2025-09-14 21:26:30 -0600

    Se creo la seccion de testimoinos

---
+++ src/JavaScript/slider.js
@@ -0,0 +1,16 @@
+const slider = document.getElementById('slider');
+const slides = slider.children;
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+let index = 0;
+
+function showSlide(i) {
+  if (i < 0) index = slides.length - 1;
+  else if (i >= slides.length) index = 0;
+  else index = i;
+  slider.style.transform = `translateX(-${index * 100}%)`;
+}
+
+prevBtn.addEventListener('click', () => showSlide(index - 1));
+nextBtn.addEventListener('click', () => showSlide(index + 1));
