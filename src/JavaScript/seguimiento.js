deedc215aff1936a705e6e009b2379d9f78041ce
Nelson Lara <nelson.lara556@hotmail.com>
2025-11-08 00:20:50 -0600

    Se creo la funcion de registro

--- src/JavaScript/seguimiento.js
+++ src/JavaScript/seguimiento.js
@@ -10,7 +10,6 @@ const contenidoCitas = document.getElementById("contenidoCitas");
 btnRegistrar.addEventListener("click", (e) =>{
         e.preventDefault()
         validarFormulario();
-        nombre()
 })
 
 function validarFormulario(){
@@ -33,6 +32,11 @@ function validarFormulario(){
                 mensaje.classList.add("bg-green-500", "text-white", "p-3", "text-center", "font-bold", "font-titulo", "mt-5", "mensajeFeedback")
                 mensaje.textContent = "Se registro correctamente"
                 formulario.appendChild(mensaje);
+                nombre()
+                email()
+                propietario()
+                fecha()
+                sintoma()
 
                 setTimeout(() => {
                        mensaje.remove()
@@ -43,8 +47,48 @@ function validarFormulario(){
 function nombre(){
         const valorNombre = inputNombre.value.trim();
         if (valorNombre !== "") {
-        const parrafo = document.createElement("p");
+        const parrafo = document.createElement("div");
         parrafo.textContent = `Paciente: ${valorNombre}`;
+        parrafo.classList.add("bg-yellow-500", "text-black", "text-center", "p-2", "rounded", "mt-2");
+        contenidoCitas.appendChild(parrafo);
+    }
+}
+
+function email(){
+        const valorEmail = inputEmail.value.trim();
+        if (valorEmail !== "") {
+        const parrafo = document.createElement("p");
+        parrafo.textContent = `Paciente: ${valorEmail}`;
+        parrafo.classList.add("bg-yellow-500", "text-black", "text-center", "p-2", "rounded", "mt-2");
+        contenidoCitas.appendChild(parrafo);
+    }
+}
+
+function propietario(){
+        const valorPropietario = inputPropietario.value.trim();
+        if (valorPropietario !== "") {
+        const parrafo = document.createElement("p");
+        parrafo.textContent = `Paciente: ${valorPropietario}`;
+        parrafo.classList.add("bg-yellow-500", "text-black", "text-center", "p-2", "rounded", "mt-2");
+        contenidoCitas.appendChild(parrafo);
+    }
+}
+
+function fecha(){
+        const valorFecha = inputFecha.value.trim();
+        if (valorFecha !== "") {
+        const parrafo = document.createElement("p");
+        parrafo.textContent = `Paciente: ${valorFecha}`;
+        parrafo.classList.add("bg-yellow-500", "text-black", "text-center", "p-2", "rounded", "mt-2");
+        contenidoCitas.appendChild(parrafo);
+    }
+}
+
+function sintoma(){
+        const valorSintoma = inputSintomas.value.trim();
+        if (valorSintoma !== "") {
+        const parrafo = document.createElement("p");
+        parrafo.textContent = `Paciente: ${valorSintoma}`;
         parrafo.classList.add("bg-yellow-500", "text-black", "text-center", "p-2", "rounded", "mt-2");
         contenidoCitas.appendChild(parrafo);
     }
